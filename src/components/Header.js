04ae37192538192ca9b83c40fc6d61b1e91d5898
cebil <cebil333@gmail.com>
2021-02-14 16:45:51 +0530

    Headbarr completed

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./Header.css";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+      <div className="main-info">
+        <h1>web developer</h1>
+        <Typed
+          className="typed-text"
+          strings={["web design", "engineer", "google add"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          contact me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
