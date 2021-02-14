e4234f8a80f69532aa7347b29ea2eae82b640128
cebil <cebil333@gmail.com>
2021-02-14 15:28:31 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>My first project</h1>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
